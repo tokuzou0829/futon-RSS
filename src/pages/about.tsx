3475b82c0b619dd9a7105fdabb991f4332d6b990
とくぞう <77785208+tokuzou0829@users.noreply.github.com>
2022-05-18 11:19:37 +0900

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -12,21 +12,18 @@ const Page: NextPage = () => {
           <h1 className="about__title">About</h1>
           <div className="about__body">
             <p>
-              このサイトはチームのためのブログスターター
+              このサイトはとくぞうの推しのRSSを取得し公開するためのサイトです.catnose氏の
+              <a href="https://github.com/catnose99/team-blog-hub">
+                Team Blog Hub
+              </a>
+              をもとに開発された
               <a href="https://github.com/yutakobayashidev/team-blog-hub-notion-api">
-                Team Blog Hub Notion API
-              </a>
-              のデモです。ブログのRSSのURLをNotion上で登録することで、チームメンバーの投稿を一覧にまとめて表示します。
+                Team Blog Hub Notin API
+              </a>  
+              を使用してこのサイトは作られています。
             </p>
             <p>
-              Medium、note、Zenn、Qiita、はてなブログなど、RSSフィードを取得できるサイトであれば、メンバーは好きな場所に投稿できます。
-            </p>
-            <p>
-              catnoseさんのリポジトリをフォークさせて頂きました。詳しくは
-              <a href="https://zenn.dev/catnose99/articles/cb72a73368a547756862">
-                チーム個々人のテックブログをRSSで集約するサイトをNext.jsで作った
-              </a>
-              をご覧ください。
+              RSSは約1時間で更新されるように設定されています。
             </p>
           </div>
           <div className="about__actions">
