8ed36a19a016e6bc552af035ae8e087747d2952a
とくぞう <77785208+tokuzou0829@users.noreply.github.com>
2022-05-19 21:48:05 +0900

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export const config = {
   },
   siteRoot:
     process.env.NODE_ENV === "production"
-      ? "https://team-blog-hub.vercel.app"
+      ? "https://futon-rss.tokuzou.click"
       : "http://localhost:3000",
   headerLinks: [
     {
